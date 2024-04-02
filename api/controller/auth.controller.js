5565e5930b10562496a4e1dd4023ae7b0f013c36
Renzaki <renzakithakuri@gmail.com>
2024-04-02 12:57:36 +0545

    adding api for signing and signingup  user

---
+++ api/controller/auth.controller.js
@@ -0,0 +1,47 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+
+export const signUp = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    if (username.length < 4) {
+      res.send(401).json({ message: "Username must be greater than 4 word" });
+    }
+    if (!email.includes("@")) {
+      res.send(401).json({ message: "Must be of email type" });
+    }
+    if (username.length < 4) {
+      res.send(401).json({ message: "Password must be greater than 4 word" });
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json({ message: "User has been created successfully!!!" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "User not found!"));
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from 'dotenv'
+import userRouter from "./routes/auth.router.js";
+dotenv.config()
+
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+app.use("/api/auth", userRouter);
+
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
-  return resizeBy.status(statusCode).json({
-    sucess: false,
+  statusCode = err.statusCode || 500;
+  message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
     statusCode,
     message,
   });
